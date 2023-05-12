57421aceb28da9619644e2e649c1dce0a0dae7e3
Rayhan Ahmed <72126224+rarayhan@users.noreply.github.com>
2023-05-12 22:18:35 +0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from expenses.views import add_expense, expense_list, user_login, user_logout
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,7 @@ urlpatterns = [
     path('add-expense/', add_expense, name='add_expense'),
     path('expense-list/', expense_list, name='expense_list'),
 ]
+
+# Add the following lines to serve static files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
